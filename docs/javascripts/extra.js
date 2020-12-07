1fe1855c9f4cf0e5488eda2884dbfc6709faeb22
Refik Can Malli <refikcanmalli@gmail.com>
2020-12-07 02:53:13 +0100

    add dark mode

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -4,3 +4,23 @@ var i;
 for (i = 0; i < algos.length; i++) {
 	pseudocode.renderElement(algos[i]);
 }
+
+var buttons = document.querySelectorAll("button[data-md-color-scheme]");
+var light_button = buttons[0];
+var dark_button = buttons[1];
+light_button.addEventListener("click", function () {
+	var attr = this.getAttribute("data-md-color-scheme");
+	document.body.setAttribute("data-md-color-scheme", attr);
+	// var name = document.querySelector("#__code_0 code span:nth-child(7)");
+	// name.textContent = attr;
+	this.style.display = "none";
+	dark_button.style.display = "block";
+});
+dark_button.addEventListener("click", function () {
+	var attr = this.getAttribute("data-md-color-scheme");
+	document.body.setAttribute("data-md-color-scheme", attr);
+	// var name = document.querySelector("#__code_0 code span:nth-child(7)");
+	// name.textContent = attr;
+	this.style.display = "none";
+	light_button.style.display = "block";
+});
